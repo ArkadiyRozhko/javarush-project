0e032d2832200651c0485c4a63dbd1a0a7ccf022
Arkadii R <arkasha9@gmail.com>
2024-03-14 12:51:24 +0200

    Задача 3707 вирішена

---
+++ src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -0,0 +1,66 @@
+package com.javarush.task.task37.task3707;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class AmigoSet<E> extends AbstractSet<E> implements Serializable,Cloneable, Set<E> {
+    private static final Object PRESENT=new Object();
+    private transient HashMap<E,Object>map;
+
+    public AmigoSet() {
+        map=new HashMap<>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        this.map = new HashMap<>(Math.max((int) (collection.size() / .75f) + 1, 16));
+        addAll(collection);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return map.put(e,PRESENT)==null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return super.contains(o);
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return super.remove(o);
+    }
+
+    public Object clone(){
+        try {
+            AmigoSet<E> newSet = (AmigoSet<E>) super.clone();
+            newSet.map = (HashMap<E, Object>) map.clone();
+            return newSet;
+        } catch (Exception e) {
+            throw new InternalError();
+        }
+    }
+    
+
+
+}
