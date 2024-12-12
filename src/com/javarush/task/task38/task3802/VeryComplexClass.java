0b778b6eb0ccf126af9a8fa9ac0f586fad61e60a
Arkadii R <arkasha9@gmail.com>
2024-12-12 10:56:05 +0200

    Задача 3802 вирішена

---
+++ src/com/javarush/task/task38/task3802/VeryComplexClass.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task38.task3802;
+
+/* 
+Проверяемые исключения (checked exception)
+*/
+
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.Scanner;
+
+public class VeryComplexClass {
+    public void veryComplexMethod() throws Exception {
+        //напишите тут ваш код
+        new FileInputStream("");
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
