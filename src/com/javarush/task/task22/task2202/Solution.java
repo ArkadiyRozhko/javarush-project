7ae314dffcb9922d1fff4f3fe3d6e7468e3487be
Ark <arkasha9@gmail.com>
2025-07-04 16:01:22 +0300

    Задача 2202 вирішена

--- src/com/javarush/task/task22/task2202/Solution.java
+++ src/com/javarush/task/task22/task2202/Solution.java
@@ -7,12 +7,24 @@ package com.javarush.task.task22.task2202;
 public class Solution {
     public static void main(String[] args) {
         System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
+        System.out.println(getPartOfString("Амиго и Диего лучшие друзья!"));
     }
 
     public static String getPartOfString(String string) {
-        return null;
+        if (string == null) {throw new TooShortStringException();}
+        String[]arr = string.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        if (arr.length <5){
+            throw new TooShortStringException();
+        }else {
+            for(int i=1;i<=4;i++){
+                sb.append(arr[i]+" ");
+            }
+        }
+        return sb.toString().trim();
     }
 
-    public static class TooShortStringException {
+    public static class TooShortStringException extends RuntimeException {
     }
 }
