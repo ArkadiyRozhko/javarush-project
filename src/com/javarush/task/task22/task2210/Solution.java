cbcd38261e5bd2ee8a344941b91ae31c7ab956be
Ark <arkasha9@gmail.com>
2025-08-05 13:49:41 +0300

    Задача 2210 вирішена

---
+++ src/com/javarush/task/task22/task2210/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task22.task2210;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/* 
+StringTokenizer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        ArrayList<String> list = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(query,delimiter);
+        while (tokenizer.hasMoreTokens()){
+            String token = tokenizer.nextToken();
+            list.add(token);
+        }
+        return list.toArray(new String[0]);
+    }
+}
